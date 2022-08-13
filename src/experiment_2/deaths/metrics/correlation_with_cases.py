0d66ce3052e53f971bf9b2a20b386b4952b2e402
Gabriel Pena <gpena@untref.edu.ar>
2022-08-13 02:45:01 -0300

    Correlations were not added

--- src/experiment_2/deaths/metrics/correlation_with_cases.py
+++ src/experiment_2/deaths/metrics/correlation_with_cases.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import scipy.stats as stats
 
-dataset = pd.read_csv('../../../../resources/data/cases/dataset_v2/dataset_2.3.csv')
+dataset = pd.read_csv('../../../../resources/data/deaths/dataset_deaths_2.3.csv')
 
 peak_magnitude = dataset['peak_magnitude_norm'].to_numpy()
-total_deaths = dataset['total_deaths_norm'].to_numpy()
+total_cases = dataset['total_cases_norm'].to_numpy()
 
-pearson_r, pearson_pv = stats.pearsonr(peak_magnitude, total_deaths)
-spearman_rho, spearman_pv = stats.spearmanr(peak_magnitude, total_deaths)
-kendall_tau, kendall_pv = stats.kendalltau(peak_magnitude, total_deaths)
+pearson_r, pearson_pv = stats.pearsonr(peak_magnitude, total_cases)
+spearman_rho, spearman_pv = stats.spearmanr(peak_magnitude, total_cases)
+kendall_tau, kendall_pv = stats.kendalltau(peak_magnitude, total_cases)
 
 print('---- Correlation metrics ----')
 print('Pearson r: {metric:.4f}    -    p-value: {pv}'.format(metric=pearson_r, pv=pearson_pv))
